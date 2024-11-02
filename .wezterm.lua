1efa1f75a42f7f75118151808fbef3fcd2ef98ff
0x00IQ <108233992+0x00IQ@users.noreply.github.com>
2024-11-02 15:02:03 +0100

    tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,9 +10,9 @@ config.animation_fps = 1
 config.cursor_blink_rate = 500
 config.term = "xterm-256color" -- Set the terminal type
 
--- config.font = wezterm.font("JetBrainsMono NF")
+config.font = wezterm.font("JetBrainsMono")
 config.cell_width = 0.9
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.7
 config.prefer_egl = true
 config.font_size = 12.0
 
@@ -25,11 +25,22 @@ config.window_padding = {
 
 -- tabs
 config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
+-- config.use_fancy_tab_bar = false
 
 
 -- keymaps
 config.keys = {
+	-- Tabs
+	{
+		key = 'T',
+		mods = 'CTRL|SHIFT|ALT',
+		action = act.SpawnTab 'CurrentPaneDomain',
+	},
+	{
+		key = 'Q',
+		mods = 'CTRL|SHIFT|ALT',
+		action = wezterm.action.CloseCurrentTab {confirm = true },
+	},
 	-- close current pane
 	{
 		key = 'x',
@@ -55,7 +66,7 @@ config.keys = {
 	},
 	-- resize panes
 	{
-		key = "h",
+		key = "H",
 		mods = "CTRL|SHIFT",
 		action = act.AdjustPaneSize({ "Left", 5 }),
 	},
@@ -77,17 +88,17 @@ config.keys = {
 	-- select panes
 	{ key = "9", mods = "CTRL", action = act.PaneSelect },
 	-- Debug overlay
-	{ key = "L", mods = "CTRL|SHIFT|ALT", action = act.ShowDebugOverlay },
+	{ key = "D", mods = "CTRL|SHIFT|ALT", action = act.ShowDebugOverlay },
 	-- toggling opacity
 	{
 		key = "O",
 		mods = "CTRL|ALT",
 		action = wezterm.action_callback(function(window, _)
 			local overrides = window:get_config_overrides() or {}
-			if overrides.window_background_opacity == 0.8 then
+			if overrides.window_background_opacity == 0.7 then
 				overrides.window_background_opacity = 0.5
 			else
-				overrides.window_background_opacity = 0.8
+				overrides.window_background_opacity = 0.7
 			end
 			window:set_config_overrides(overrides)
 		end),
@@ -95,47 +106,14 @@ config.keys = {
 }
 
 -- color scheme
-config.color_scheme = 'Gogh (Gogh)'
-
-config.colors = {
-	background = "#0c0b0f", -- dark purple
-	cursor_border = "#bea3c7",
-	cursor_bg = "#bea3c7",
-
-	tab_bar = {
-		background = "#0c0b0f",
-		-- background = "rgba(0, 0, 0, 0%)",
-		active_tab = {
-			bg_color = "#0c0b0f",
-			fg_color = "#bea3c7",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-		inactive_tab = {
-			bg_color = "#0c0b0f",
-			fg_color = "#f8f2f5",
-			intensity = "Normal",
-			underline = "None",
-			italic = false,
-			strikethrough = false,
-		},
-
-		new_tab = {
-			bg_color = "#0c0b0f",
-			fg_color = "white",
-		},
-	},
-}
+config.color_scheme = 'Catppucin Mocha'
 
 config.window_frame = {
-	-- font = wezterm.font({ family = "JetBrainsMono NF", weight = "Light" }),
+	font = wezterm.font({ family = "JetBrainsMono", weight = "Light" }),
 	active_titlebar_bg = "#0c0b0f",
 }
 
--- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
-config.window_decorations = "NONE | RESIZE"
+-- config.window_decorations = "RESIZE"
 config.default_prog = { "bash" }
 config.initial_cols = 80
 
