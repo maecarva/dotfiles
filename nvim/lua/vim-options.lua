91649b8b359fb66206e81199c891d35a5c88ef1b
0x00IQ <108233992+0x00IQ@users.noreply.github.com>
2024-11-02 16:02:16 +0100

    neovim

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,9 @@
+-- base config
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set relativenumber")
+vim.cmd("set number")
+vim.cmd("set autoindent")
+vim.g.mapleader= " "
+vim.g.maplocalleader = "\\"
