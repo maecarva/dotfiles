402cd42dd73b84451b74d816ab325b3abaee84f6
maecarva <mael.crv@gmail.com>
2025-06-22 19:13:50 +0200

    ok

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,4 +1,12 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
+	'stevearc/conform.nvim',
+	opts = {
+		formatters_by_ft = {
+			go = { "goimports" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
 }
