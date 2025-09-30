ad32375dfc7073bb92128a2b16c79fc3c9015421
Mael Carvalho <mael.crv@gmail.com>
2025-09-30 20:50:24 +0200

    clang-format

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -15,8 +15,8 @@ return {
 			yaml = { "prettierd", "prettier" },
 			markdown = { "prettierd", "prettier" },
 			css = { "prettierd", "prettier" },
-			c = {},
-			cpp = {},
+			cpp = { "clang-format" },
+			c = { "clang-format" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
