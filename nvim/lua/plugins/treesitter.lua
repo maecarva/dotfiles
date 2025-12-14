c05ebf7e18fd382c2925dd36f9f8fa9ba7e56112
mael <mael.crv@gmail.com>
2025-12-14 22:18:13 +0100

    templ

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,26 +12,25 @@
 -- 	end
 -- }
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        parser_config.c3 = {
-            install_info = {
-                url = "https://github.com/c3lang/tree-sitter-c3",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "main",
-            },
-            filetype = "c3",
-        }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.c3 = {
+			install_info = {
+				url = "https://github.com/c3lang/tree-sitter-c3",
+				files = { "src/parser.c", "src/scanner.c" },
+				branch = "main",
+			},
+			filetype = "c3",
+		}
 
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "lua", "c", "cpp", "rust", "html", "javascript", "typescript", "go", "odin", "c3", "powershell" },
-            
-            highlight = { enable = true },
-            indent = { enable = true },
-            
-        })
-    end
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "c", "cpp", "rust", "html", "javascript", "typescript", "go", "zig", "templ" },
+			highlight = { enable = true },
+			indent = { enable = true },
+
+		})
+	end
 }
